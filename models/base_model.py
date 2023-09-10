0c022f9ed9b99624eff1d131042510d1077e4bb7
FRIDAUS <ayobamifridaus@gmail.com>
2023-09-10 11:58:07 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
         else:
             for key, value in kwargs.items():
@@ -48,7 +49,6 @@ class BaseModel:
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.new(self.to_dict())
         storage.save()
 
     def to_dict(self):
