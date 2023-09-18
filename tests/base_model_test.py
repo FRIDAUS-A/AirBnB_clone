9df6c36b2318a756caf0c2a62db4b7fecd202308
FRIDAUS <fridausokoya@gmail.com>
2023-09-18 13:51:06 +0100

    test for the BaseModel class

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -1,62 +1,87 @@
 #!/usr/bin/env python3
 """Test cases for the BaseModel class"""
-
 import unittest
 from models.base_model import BaseModel
 
-class Test_BaseModel(unittest.TestCase):
-    """Testcase class for the BaseModel class"""
+class test_instantiation_BaseModel(unittest.TestCase):
+    """Testcase class for testing BaseModel instatiation"""
+    def setUp(self):
+        """called before running the test"""
+        self.obj1 = BaseModel()
+        self.obj2 = BaseModel()
 
-    def test_idstring(self):
-        """Test if id is a string"""
+    def tearDown(self):
+        """called after running the test"""
+        self.obj1 = None
+        self.bj2 = None
+
+    def test_BaseModel(self):
+        """test if the object is an instance of the BaseModel class"""
+        self.assertIsInstance(self.obj1, BaseModel)
+
+    def test_id_string(self):
+        """test if id is string"""
+        self.assertIsInstance(self.obj1.id, str)
+
+    def test_id_unique(self):
+        """test if id unique"""
+        self.assertNotEqual(self.obj1.id, self.obj2.id)
+    
+    def test_created_at_unique(self):
+        """test if the created time is different"""
+        self.assertNotEqual(self.obj1.created_at, self.obj2.created_at)
+
+    def test_created_updated(self):
+        """test if the created_at and updated_at is the same when first created"""
+        self.assertNotEqual(self.obj1.created_at, self.obj1.updated_at)
+
+    def test_updated_after_save(self):
+        """test if the timse is updated after saving"""
         obj = BaseModel()
-        self.assertIsInstance(obj.id, str)
+        time = obj.updated_at
+        obj.save()
+        time_after = obj.updated_at
+        self.assertNotEqual(time, time_after)
 
-    def test_uniqueid(self):
-        """Test if the id is unique"""
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        self.assertNotEqual(obj1.id, obj2.id)
-
-    def test_created_at(self):
-        """check if the time created is different"""
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        self.assertNotEqual(obj1.created_at, obj2.created_at)
-
-    def test_updated_at_initially(self):
-        """check if the time the updated was first given is
-        different for different obj cases
-        """
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        self.assertNotEqual(obj1.updated_at, obj2.updated_at)
-
-    def test_unique_created_updated(self):
-        """check if the created time and updated time is different"""
+    def test_created_at_isthesame(self):
+        """test if the created time is constant"""
         obj = BaseModel()
-        self.assertNotEqual(obj.created_at, obj.updated_at)
+        time = obj.created_at
+        obj.save()
+        time_after = obj.created_at
+        self.assertEqual(time, time_after)
 
-    def test_updated_at_saved(self):
-        """check if it the time updated is differnt when saved"""
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        obj1.save()
-        obj2.save()
-        self.assertNotEqual(obj1.updated_at, obj2.created_at)
+class test_string_representation(unittest.TestCase):
+    """test the string representation of object"""
+    maxDiff = None
 
-    def test_to_dict(self):
-        """Test the return value of to_dict() method"""
-        my_model = BaseModel()
-        id = my_model.id
-        created_at = str(my_model.created_at.isoformat())
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        my_model.save()
-        updated_at = str(my_model.updated_at.isoformat())
-        my_model_json = my_model.to_dict()
-        expected_result_todict = {'my_number': 89, 'name': 'My First Model', '__class__': 'BaseModel', 'updated_at': updated_at, 'id': id, 'created_at': created_at}
-        self.assertEqual(my_model_json, expected_result_todict)
+    def setUp(self):
+        """called before running the test"""
+        self.obj = BaseModel()
 
-if __name__ == "__main__":
+    def tearDown(self):
+        """called after running the test"""
+        self.obj = None
+
+    def test_string(self):
+        """test string"""
+        string = f"[{self.obj.__class__.__name__}] ({self.obj.id}) {self.obj.__dict__}"
+        self.assertEqual(self.obj.__str__(), string)
+
+class test_instance_method(unittest.TestCase):
+    """test the instance methods"""
+
+    def setUp(self):
+        """called before running the test"""
+        self.obj = BaseModel()
+
+    def tearDown(self):
+        """called after running the test"""
+        self.obj = None
+
+    def test_dict_class_(self):
+        """test the to_dict method for class name"""
+        self.assertEqual(self.obj.__class__.__name__, self.obj.to_dict()["__class__"])
+
+if __name__ == '__main__':
     unittest.main()
